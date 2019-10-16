cc9167a851cbaa7a316b430350d23585727e102f
George Steblovsky <steblg@gmail.com>
2019-10-15 20:54:16 -0400

    converting to data.tree, print, plot, predict, etc

--- R/tree.R
+++ R/tree.R
@@ -1,11 +1,10 @@
-# Inspired by:
-# https://www.statworx.com/de/blog/coding-regression-trees-in-150-lines-of-code/
 
-# Any split 's' devides split region into x < s and x >= s
 
 # ToDo:
 #   1. Handling of NA's. For now one has to deal with them (na.omit) prior to using the method;
-
+#   2. Feature importance
+#   3. Complexity pruning
+#   4. Cross-validation
 
 
 rss <- function(x) var(x) * (length(x) - 1)
@@ -75,7 +74,6 @@ gsTree <- function(formula, input, model.control= list(minS = 20, minD = 5, erro
       } else {
         split_conditions_str <- paste(node$split_conditions, collapse = " & ")
         nodeF <- eval(str2lang(split_conditions_str), envir=X)  # not finished (*)
-        # browser()
         XX <- X[nodeF, ]
         YY <- Y[nodeF]
       }
@@ -88,14 +86,14 @@ gsTree <- function(formula, input, model.control= list(minS = 20, minD = 5, erro
 
     findBestNodeToSplit <- function(nodes){
       split_effect <- sapply(nodes, function(i){
-        return(rtree[[i]]$error - sum(rtree[[i]]$best_split$error))
+        return(rtree_[[i]]$error - sum(rtree_[[i]]$best_split$error))
       })
       return(nodes[which.max(split_effect)])
     }
 
     createSplit <- function(i){
-      node_to_split <- rtree[[i]]
-      N <- length(rtree)
+      node_to_split <- rtree_[[i]]
+      N <- length(rtree_)
 
       children <- list()
       op <- with(node_to_split, best_split$op)
@@ -112,9 +110,7 @@ gsTree <- function(formula, input, model.control= list(minS = 20, minD = 5, erro
           nN = N + j,
           parent = i
         )
-        # browser()
         add_condition <- with(node_to_split, list(feature=X_label[best_split$feature], operation = op[j], split_value=best_split$split))
-        # child$split_conditions_list <- c(node_to_split$split_conditions_list, list(add_condition))
         split_condition_str <- c(node_to_split$split_conditions, with(add_condition, paste(feature, operation, split_value)))
         childF <- eval(str2lang(paste(split_condition_str, collapse = ' & ')), envir = X)
         
@@ -141,12 +137,17 @@ gsTree <- function(formula, input, model.control= list(minS = 20, minD = 5, erro
     markNodeLeaf <- function(node){
       node$status <- 'L'
       node['best_split'] <- list(NULL)
+      return(node)
     }
-
+   
     # Tree to be grown
-    rtree <- list()
+    rtree_ <- list()
+    dtree_ <- NULL
+    leaves_ <- NULL
     # error value of the model
     errorVal <- minFUN(Y)
+    
+    
 
     root_node <- list(
       nN = 1,                        # Node number
@@ -161,12 +162,13 @@ gsTree <- function(formula, input, model.control= list(minS = 20, minD = 5, erro
       # error_right of the right child node
       obsN = nrow(input)           # Number of observations for this Node
     )
+    
     tmp <- bestNodeSplit(root_node)
     if(is.null(tmp)) { # no allowed splits
       stop("Tree can't be build under current conditions, try changing model.control argument")
     }
     root_node['best_split'] <- list(tmp)
-    rtree[[1]] <- root_node
+    rtree_[[1]] <- root_node
 
     nodesN_to_split <- 1
     while(length(nodesN_to_split) > 0){
@@ -174,26 +176,137 @@ gsTree <- function(formula, input, model.control= list(minS = 20, minD = 5, erro
       cat("Nodes that can be split: ", paste(nodesN_to_split, collapse = ", "), "\n")
 
       best_split <- findBestNodeToSplit(nodesN_to_split)
-      # if(best_split == 6) browser()
       cat("Best split: ", paste(best_split, collapse = ", "), "\n")
       # update parent
-      rtree[[best_split]]$status<- 'P'
-      nodeToSplit <- rtree[[best_split]]
+      rtree_[[best_split]]$status<- 'P'
+      nodeToSplit <- rtree_[[best_split]]
       deltaError <- with(nodeToSplit, error - sum(best_split$error))
       if(100 * deltaError / errorVal[length(errorVal)] < model.control$minD){
-        for(i in nodesN_to_split) rtree[[i]] <- markNodeLeaf(rtree[[i]])
+        for(i in nodesN_to_split) rtree_[[i]] <- markNodeLeaf(rtree_[[i]])
         break
       } else {
         children <- createSplit(best_split)
-        rtree <- c(rtree, children)
-        errorVal <- c(errorVal, sum(sapply(rtree, function(x){
+        rtree_ <- c(rtree_, children)
+        errorVal <- c(errorVal, sum(sapply(rtree_, function(x){
           if(x$status == 'P') return(0) else return(x$error)
         })))
       }
-      nodesN_to_split <- seq_len(length(rtree))[sapply(rtree, function(x) x$status == 'S')]
+      nodesN_to_split <- seq_len(length(rtree_))[sapply(rtree_, function(x) x$status == 'S')]
 
     }
-    return(list(tree = rtree, errVal = errorVal))
+    
+    nodeDefn <- function(node){
+      slL <- list()
+      for(e in node$split_conditions_list){
+        slL[[e$feature]] <- c(slL[[e$feature]], list(e))
+      }
+      collapse_feature <- function(cL){ # elements of cL have the same 'feature'
+          if(length(cL) == 1) return(cL)
+          opF <- factor(sapply(cL, function(x) x$operation))
+          L <- split(cL, opF)
+          names(L) <- NULL
+          collapse_F <- function(y) { # elements of y have same 'feature' and the same 'operation'
+            if(y[[1]]$operation %in% c("==", '!=')) return(y)
+            if(y[[1]]$operation == "<") return(list(feature = y[[1]]$feature, operation = y[[1]]$operation, split_value = min(sapply(y, function(x) x$split_value))))
+            if(y[[1]]$operation == ">=") return(list(feature = y[[1]]$feature, operation = y[[1]]$operation, split_value = max(sapply(y, function(x) x$split_value))))
+            stop(paste(paste("'", y[[1]]$operation, "'", sep =""), "is not supported"))
+          }
+          return(lapply(L, collapse_F))
+      }
+      return(lapply(slL, collapse_feature))
+    }
+    
+    nodeDefnStr <- function(node){
+      definition <- unlist(nodeDefn(node = node), recursive = FALSE, use.names = FALSE)
+      return(paste(sapply(definition, function(x) with(x, paste("(", feature, operation, split_value, ")"))), collapse = " & "))
+    }
+        
+    toDataTreeNode <- function(rtree_node, abbrev = TRUE) {
+      library(data.tree)
+
+      if(isTRUE(abbrev)){
+       # nm <- paste(rtree_node$split_conditions_list[[length(rtree_node$split_conditions_list)]], collapse = " ")
+        nm <- with(rtree_node, split_conditions[length(split_conditions)])
+      } else {
+        nm <- nodeDefnStr(rtree_node)
+      }
+      rv <- Node$new(nm)
+      rv$parent_nN <- rtree_node$parent
+      for(nm in setdiff(names(rtree_node), 'parent')) rv[[nm]] <- rtree_node[[nm]]
+      rv
+    }
+    
+    toDataTree <- function(tri){
+      library(data.tree)
+      
+      flat_tree <- lapply(tri, toDataTreeNode)
+      assembled_tree <- flat_tree[[1]]
+      for(i in 2 : length(flat_tree)){
+        flat_tree[[flat_tree[[i]]$parent_nN]]$AddChildNode(flat_tree[[i]])
+      }
+      return(assembled_tree)
+    }
+    
+    dataTree <- function(){
+      if( is.null(dtree_) ) dtree_ <<- toDataTree(rtree_)
+      return(dtree_)
+    }
+    
+    printTree <- function(){
+      base::print(dataTree(), "obsN", "value", "error")
+    }
+    
+    extractLeaves <- function(as.dataframe = FALSE){
+      if(is.null(leaves_)){
+        leaves_index <- seq_along(rtree_)[sapply(rtree_, function(x) x$status == 'L')]
+        leaves <- lapply(leaves_index, function(x) {
+          xn <- rtree_[[x]]
+          xn[["defn"]] <- nodeDefnStr(xn)
+          xn <- xn[c("nN", "split_conditions", "value", "error", "obsN", "defn")]
+          xn
+        })
+        leaves_ <<- leaves
+      }
+      if(isTRUE(as.dataframe)){
+        rv <- lapply(leaves_, function(x) {
+          x <- within(x, split_conditions <- paste(split_conditions, collapse = " & "))
+          x <- as.data.frame(x, stringsAsFactors = FALSE)
+          row.names(x) <- NULL
+          x
+        })
+        return(do.call(rbind, rv))
+      }
+      return(leaves_)
+    }
+        
+    printLeaves <- function(){
+      print(extractLeaves(as.dataframe = TRUE))
+    }
+    
+    prediction <- function(X){
+      leaves <- extractLeaves(as.dataframe = TRUE)
+      Y <- vector("list", nrow(X))
+      for(i in seq_len(nrow(leaves))){
+        nodeF <- eval(str2lang(leaves[i, "split_conditions"]), envir=X)
+        Y[nodeF] <- leaves[i, 'value']
+      }
+      return(unlist(Y))
+    }
+    
+    plotTree <- function(){
+      plot(dataTree())
+    }
+    
+    
+    rv <- list(
+      tree = rtree_,
+      errVal = errorVal,
+      printTree = printTree,
+      printLeaves = printLeaves,
+      plotTree = plotTree,
+      predict = prediction
+    )
+    return(rv)
   }
 
   #Example:
@@ -204,4 +317,11 @@ gsTree <- function(formula, input, model.control= list(minS = 20, minD = 5, erro
   set.seed(1)
   train <- sample(seq_len(nrow(Boston)), nrow(Boston)/2)
 
+  #foo <- gsTree(medv ~ ., input = Boston[train, ], model.control=list(minS = 5, minD = 0))
   foo <- gsTree(medv ~ ., input = Boston[train, ])
+  Y <- foo$predict(Boston[-train, ])
+
+  fit <- tree(medv ~ ., data = Boston[train, ])
+  YY <- predict(fit, newdata = Boston[-train, ])
+  
+  data.frame(Y = Y, YY = YY, YYY = Boston[-train, 'medv'])
